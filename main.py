254ad7715bae93610b1e23a7a840818eecae2b0c
LamTuyetNhu <lamtuyetnhu2908@gmail.com>
2024-02-27 16:38:59 +0700

    nhan dien

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ import os
 # print("ok")
 # dfs = DeepFace.find(img_path = "face3.jpg", db_path = "./faces/", model_name='VGG-Face')
 # print(dfs)
+
 def recognize_images(source_folder, face_db_folder):
+    total_similarity = 0
+    total_images = 0
+
     # Duyệt qua từng tập tin ảnh trong thư mục nguồn
     for root, dirs, files in os.walk(source_folder):
         for file_name in files:
@@ -21,10 +25,22 @@ def recognize_images(source_folder, face_db_folder):
                 # Nếu có ít nhất một kết quả
                 if result:
                     # In ra độ chính xác của kết quả đầu tiên
-                    print("Độ chính xác:", result[0]['Similarity'])
+                    similarity = result[0]['Similarity']
+                    print("Độ chính xác:", similarity)
+
+                    # Cộng dồn độ chính xác cho tổng
+                    total_similarity += similarity
+                    total_images += 1
+
+    # Tính trung bình độ chính xác
+    if total_images > 0:
+        average_similarity = total_similarity / total_images
+        print(f"\nTrung bình độ chính xác cho tập test là: {average_similarity}")
+    else:
+        print("\nKhông có ảnh nào trong tập test.")
 
 if __name__ == "__main__":
-    source_folder = "/content/Face Data/Face Dataset"
-    face_db_folder = "/content/Face Data/Data_Test" # Thư mục chứa ảnh khuôn mặt để so sánh
+    source_folder = "/content/Face Data/Data_Test"
+    face_db_folder = "/content/Face Data/Face Dataset"  # Thư mục chứa ảnh khuôn mặt để so sánh
 
     recognize_images(source_folder, face_db_folder)
